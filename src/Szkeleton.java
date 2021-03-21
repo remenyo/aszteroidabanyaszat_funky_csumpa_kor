fd6ce66462f0e5a576898226ada5c49198e511ea
Englert Dániel <edani0050@gmail.com>
2021-03-21 15:20:35 +0100

    Beni beollozva

--- src/Szkeleton.java
+++ src/Szkeleton.java
@@ -64,4 +64,68 @@ public class Szkeleton {
 		t.beallitAszteroida(regi);
 		t.Mozgas();
 	}
+	
+	public void NyersanyagVisszahelyezésMenu() {
+		System.out.println("1. Urán Visszahelyzés\r\n"
+				+ "2. Vízjég Bányászat\r\n"
+				+ "3. Szén Bányászat\r\n"
+				+ "4. Vas Bányászat");
+	}
+	
+	public void mozgasTeleporttal() 
+	{
+		Nap nap = new Nap();
+		Aszteroida a = new Aszteroida(3,false,nap,new Szen());
+		Portal p2 = new Portal();
+		Aszteroida b = new Aszteroida(3,false,nap,new Szen());
+		Portal p = new Portal();
+		Telepes t = new Telepes();
+		t.beallitAszteroida(a);
+		a.hozzaadSzereplo(t);
+		p.beallitVegpont(a);
+		p2.beallitVegpont(b);
+		p.beallitPar(p2);
+		p2.beallitPar(p);
+		p.Utazas(t);	
+	}
+	
+	public void uranBanyaszat() 
+	{
+		Nap nap = new Nap();
+		Telepes t = new Telepes();
+		Aszteroida a = new Aszteroida(3,false,nap,new Uran());
+		t.beallitAszteroida(a);
+		a.hozzaadSzereplo(t);
+		a.Banyaszat();
+	}
+	
+	public void vasBanyaszat() 
+	{
+		Nap nap = new Nap();
+		Telepes t = new Telepes();
+		Aszteroida a = new Aszteroida(3,false,nap,new Vas());
+		t.beallitAszteroida(a);
+		a.hozzaadSzereplo(t);
+		a.Banyaszat();
+	}
+	
+	public void vizjegBanyaszat() 
+	{
+		Nap nap = new Nap();
+		Telepes t = new Telepes();
+		Aszteroida a = new Aszteroida(3,false,nap,new Vizjeg());
+		t.beallitAszteroida(a);
+		a.hozzaadSzereplo(t);
+		a.Banyaszat();
+	}
+	
+	public void szenBanyaszat() 
+	{
+		Nap nap = new Nap();
+		Telepes t = new Telepes();
+		Aszteroida a = new Aszteroida(3,false,nap,new Szen());
+		t.beallitAszteroida(a);
+		a.hozzaadSzereplo(t);
+		a.Banyaszat();
+	}
 }
