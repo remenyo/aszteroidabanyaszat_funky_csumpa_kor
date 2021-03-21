90413740472182341d457d4be5864bf72ff73bbb
remenyo <remenyo@gmail.com>
2021-03-21 19:27:02 +0100

    Made Log constructor private

--- src/Log.java
+++ src/Log.java
@@ -2,9 +2,11 @@ package src;
 
 public class Log {
 
-    // https://stackoverflow.com/a/4065546 az alap innen jött
+    private Log() {
+    }
 
     private static StackTraceElement getCaller() {
+        // https://stackoverflow.com/a/4065546 az alap innen jött
         return Thread.currentThread().getStackTrace()[4]; // ?? magic
     }
 
@@ -129,31 +131,3 @@ public class Log {
     public static final String WHITE_BACKGROUND_BRIGHT = "\033[0;107m"; // WHITE
 
 }
-
-/*
- * import java.util.logging.FileHandler; import java.io.IOException; import
- * java.util.logging.ConsoleHandler; import java.util.logging.Logger; import
- * java.util.logging.Level; import java.util.logging.SimpleFormatter;
- * 
- * public class Log { private final static Logger logger =
- * Logger.getLogger(Logger.GLOBAL_LOGGER_NAME); private static FileHandler fh =
- * null; private static ConsoleHandler ch = null; static boolean already_ran =
- * false;
- * 
- * public static Level DEBUG = Level.CONFIG; public static Level INFO =
- * Level.INFO; public static Level WARNING = Level.WARNING; public static Level
- * ERROR = Level.SEVERE;
- * 
- * // TODO add log function, add Level constants
- * 
- * static void init() { if (already_ran) { System.out.
- * println("CSUMPALOGGER WAS INITIALIZED MORE THAN ONCE... DID NOTHING");
- * return; } try { fh = new FileHandler("loggerExample.log", false); } catch
- * (IOException e) {
- * System.out.println("FILEHANDLER COULD NOT INITIALIZE LOG FILE"); } ch = new
- * ConsoleHandler(); ch.setFormatter(new SimpleFormatter()); fh.setFormatter(new
- * SimpleFormatter()); logger.addHandler(fh); logger.addHandler(ch);
- * logger.setLevel(Level.ALL); already_ran = true; }
- * 
- * }
- */
