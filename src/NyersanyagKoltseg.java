73f8d8050c9aae96302a36d60c6c7fc435462ff2
PoPovok <70849892+PoPovok@users.noreply.github.com>
2021-03-21 16:31:38 +0100

    gruh

--- src/NyersanyagKoltseg.java
+++ src/NyersanyagKoltseg.java
@@ -4,27 +4,44 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class NyersanyagKoltseg {
-ArrayList<Nyersanyag> szuksegesek; 
+ArrayList<Nyersanyag> koltseg; 
+ArrayList<Nyersanyag> masolatTarolo; 
+
     
     public void hozzaadNyersanyag(Nyersanyag ny)
      {
-         szuksegesek.add(ny);
+         koltseg.add(ny);
      }
     
-    public boolean Szukseges(Nyersanyag ny)
-    {
+    //public void Szukseges(Nyersanyag ny)
+    //{
         
-    }
+    //}
     
-    public boolean koltsegSzamitas(ArrayList<Nyersanyag> ny)
-    {
-        Scanner in = new Scanner(System.in);
-        return new Szkeleton().Kerdes("Meg van nála az összes szükséges nyersanyag? (1:Igen 0:Nem)");
+    //public boolean koltsegSzamitas(ArrayList<Nyersanyag> ny)
+    //{
+    //    Scanner in = new Scanner(System.in);
+    //    return new Szkeleton().Kerdes("Meg van nála az összes szükséges nyersanyag? (1:Igen 0:Nem)");
+    //}
+    
+    public void koltsegSzamitas(ArrayList<Nyersanyag> ny) {
+    	ArrayList<Nyersanyag> koltsegKlon = koltseg;
+    	masolatTarolo = ny;
+    	
+    	for(Nyersanyag m : masolatTarolo) {
+    		for(Nyersanyag k : koltsegKlon) {
+    			if(m.azonos(k)) {
+    				
+    			}
+    		}
+    	}
+    	
+    	
     }
     
     public void felulirNyersanyagok(ArrayList<Nyersanyag> ny)
     {
-        szuksegesek = ny;
+        koltseg = ny;
     }
      
 }
