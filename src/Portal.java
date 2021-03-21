8f8d51d67424df320974a1b34374a6e442e26ddb
PoPovok <70849892+PoPovok@users.noreply.github.com>
2021-03-21 13:34:00 +0100

    portal robot
    bruh

--- src/Portal.java
+++ src/Portal.java
@@ -1,5 +1,53 @@
 package src;
 
 public class Portal {
-
+	private Boolean aktiv=false;
+	private Telepes birtoskos;
+	private Portal par;
+	private Aszteroida vegpont;
+	
+	public void Robbanas() {
+		par.Beszippant();
+		Beszippant();
+	}
+	
+	void beallitAktiv() {
+		aktiv=true;
+	}
+	
+	public Aszteroida getVegpont() {
+		return vegpont;
+	}
+	public void beallitVegpont(Aszteroida a) {
+		vegpont=a;
+		birtokos.torolPortal(this);
+		birtokos=null;
+		vegpont.hozzaadSzomszed(this);
+		
+		mukodesbeHelyezes(a);
+	}
+	public void beallitPar(Portal p) {
+		par=p;
+	}
+	private void mukodesbeHelyezes(Aszteroida a) {
+		if(par.getVegpont()!=null) {
+			beallitAktiv();
+			par.beallitAktiv();
+		}
+			
+	}
+	private void Beszippant() {
+		if(birtokos!=null)
+			birtokos.torolPortal(this);
+		if(vegpont!=null)
+			vegpont.torolSzomszed(this);
+			
+	}
+	public void Utazas(Szereplo sz) {
+		if(par!=null)
+			par.Teleportalas(sz);
+	}
+	public void Teleportalas(Szereplo sz) {
+		vegpont.Utazas(sz);
+	}
 }
--- src/Robot.java
+++ src/Robot.java
@@ -1,5 +1,34 @@
 package src;
 
-public class Robot {
+import java.util.Random;
 
+public class Robot {
+	public void Robbanas() {
+		mozgasIntelligencia();
+	}
+	public void Lepes() {
+		Random rand = new Random();
+		int cselekves = rand.nextInt(4);
+		
+		//mekkora eséllyel fúrjon vagy mozogjon
+		//(fúrás gyakrabb mint a mozgás)
+		if(cselekves%4==0)
+			mozgasIntelligencia();
+		else
+			Furas();
+		
+	}
+	
+	//Ne legyen kódismétlés
+	//felhasználásra kerül mindkét függvényben
+	public void mozgasIntelligencia() {
+		Random rand = new Random();
+		int szomszedszam = aszteroida.getSzomszedSzam();
+		//Aszteroidába egy függvény, ami lekérdezi a szomszédok tömb hosszát;
+		//szükséges az Aszteroida felrobbanásához, ha az aszteroidának nem maradtak szomszédai
+		//~~Analízis model2 - 4.4.2 szekvencia
+		
+		Mozgas( rand.nextInt(szomszedszam) ); //véletlenszerûen vándorlik egy szomszédra
+	}
+	
 }
