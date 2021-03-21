867ddf8111dbbe44c97b1ba1147329a4d3b59e3b
remenyo <remenyo@gmail.com>
2021-03-21 14:01:02 +0100

    szen, + app

---
+++ src/Szen.java
@@ -0,0 +1,14 @@
+package src;
+
+public class Szen extends Nyersanyag {
+
+    Szen() {
+        super("Szén");
+    }
+
+    @Override
+    public void felszinreKerul(Aszteroida a) {
+        // do nothing;
+    }
+
+}
