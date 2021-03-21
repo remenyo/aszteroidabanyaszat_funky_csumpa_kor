dd06ca066c4bd9cd17c7e3b8e3c5709a20d6705e
Benana6 <szeman.benjamin@gmail.com>
2021-03-21 16:00:32 +0100

    Update Beniakiralyskeleton23456.java

--- src/Beniakiralyskeleton23456.java
+++ src/Beniakiralyskeleton23456.java
@@ -6,6 +6,10 @@ public class Beniakiralyskeleton23456 {
 	{
 		Robot r = new Robot();
 		Aszteroida a = new Aszteroida(2,false,new Nap(),new Uran());
+		Aszteroida b = new Aszteroida(2,false,new Nap(),new Uran());
+		Portal p = new Portal();
+		a.hozzaadSzomszed(b);
+		a.hozzaadSzomszed(p);
 		a.Furas();
 	
 			
