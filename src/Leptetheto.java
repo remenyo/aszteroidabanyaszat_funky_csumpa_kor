884b6f782362ca63959dbe30af98c3c60d9a502b
Englert Dániel <edani0050@gmail.com>
2021-03-22 10:30:34 +0100

    Update Leptetheto.java

--- src/Leptetheto.java
+++ src/Leptetheto.java
@@ -1,5 +1,6 @@
 package src;
 
+//Azok az osztályok valósítják meg, amelyek minden körben lépnek
 public interface Leptetheto {
-	public void Lepes();
+  public void Lepes(); //mi történjen egy lépésben
 }
