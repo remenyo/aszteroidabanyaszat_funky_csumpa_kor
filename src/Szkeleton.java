2e219ad0f20dbc75dc0ed38d9a65ec9262674ed1
remenyo <remenyo@gmail.com>
2021-04-16 22:01:51 +0200

    add getID

--- src/Szkeleton.java
+++ src/Szkeleton.java
@@ -7,6 +7,7 @@ import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.TreeMap;
+import java.util.Map.Entry;
 import java.util.Map;
 
 public class Szkeleton {
@@ -28,7 +29,7 @@ public class Szkeleton {
 
 	public static void Fomenu() {
 		int valasz = Cin.kerdez_tobbvalasz("Fõmenü", "Játék indítás", "Parancssor",
-				"Teszt betöltés", "Kilépés");
+				"Teszt betöltés", "Játék alaphelyzetbe állítása", "Kilépés");
 		switch (valasz) {
 			case 1:
 				Jatek.jatekInditas();
@@ -38,6 +39,10 @@ public class Szkeleton {
 			case 3:
 				Log.info("tinglitangli");
 			case 4:
+				if (Cin.getBool("Tuti?")) {
+					Log.info("igazi rosszfiúval van dolgunk");
+				}
+			case 5:
 				return;
 			default:
 				break;
@@ -94,7 +99,7 @@ public class Szkeleton {
 		// van ilyen objektumunk tárolva véletlen?
 		if (objektumok.containsKey(ertek.toLowerCase())) {
 			if (tipus.isInstance(objektumok.get(ertek))) {
-				
+
 				return objektumok.get(ertek);
 			}
 		}
@@ -270,6 +275,15 @@ public class Szkeleton {
 		return "";
 	}
 
+	public static String getID(Object object) {
+		for (Entry<String, Object> e : objektumok.entrySet()) {
+			if (e.getValue().equals(object)) {
+				return e.getKey();
+			}
+		}
+		return "<ismeretlen_objektum>";
+	}
+
 	public static void JatekMenu() {
 		switch (Cin.kerdez_tobbvalasz("MENÜ", "Mozgás ûrhajóval", "Mozgás teleport kapun keresztül",
 				"Bányászat", "Vízjég Fúrás", "Urán fúrás", "Fúrás vas", "Portálkapu építés",
@@ -303,283 +317,55 @@ public class Szkeleton {
 				break;
 		}
 	}
-	// Az elején levõ inicializálás mindenhol a megfelelõ mûködés érdekében van.
-	public static void MozgasUrhajoval() {
-		Telepes t = new Telepes();
-		Aszteroida regi = new Aszteroida(1, true, new Nap(), new Uran());
-		Aszteroida uj = new Aszteroida(1, true, new Nap(), new Uran());
-		regi.hozzaadSzomszed(uj);
-		uj.hozzaadSzomszed(regi);
-		regi.hozzaadSzereplo(t);
-		t.beallitAszteroida(regi);
-		t.Mozgas(0);
+
+	static void teszt_letrehozPortalAszteroida(String... argumentumok) {
+		letrehoz("Portal", argumentumok[0]);
+		beallit(argumentumok[0], "aszteroida", argumentumok[1]);
 	}
 
-	public static void VasVisszahelyez() {
-		Aszteroida a = new Aszteroida(1, true, new Nap(), null);
-		Vas v = new Vas();
-		Telepes t = new Telepes();
-		a.hozzaadSzereplo(t);
-		t.beallitAszteroida(a);
-		t.hozzaadNyersanyag(v);
-		t.visszarakNyersanyag(v);
+	static void teszt_osszekotAszteroida(String... argumentumok) {
+		hiv(argumentumok[0], "hozzaadSzomszed", argumentumok[1]);
+		hiv(argumentumok[1], "hozzaadSzomszed", argumentumok[0]);
 	}
 
-	public static void SzenVisszahelyez() {
-		Aszteroida a = new Aszteroida(1, true, new Nap(), null);
-		Szen sz = new Szen();
-		Telepes t = new Telepes();
-		a.hozzaadSzereplo(t);
-		t.beallitAszteroida(a);
-		t.hozzaadNyersanyag(sz);
-		t.visszarakNyersanyag(sz);
+	static void teszt_osszekotPortal(String... argumentumok) {
+		hiv(argumentumok[0], "beallitPar", argumentumok[1]);
+		hiv(argumentumok[1], "beallitPar", argumentumok[0]);
 	}
 
-	public static void UranVisszahelyezes() {
-		Aszteroida a = new Aszteroida(1, true, new Nap(), null);
-		Aszteroida b = new Aszteroida(1, true, new Nap(), new Vas());
-		Uran u = new Uran();
-		Telepes t = new Telepes();
-		Telepes t2 = new Telepes();
-		t2.setPortal(new Portal());
-		t.hozzaadNyersanyag(new Szen());
-		t.setPortal(new Portal());
-		t2.hozzaadNyersanyag(new Szen());
-		b.hozzaadSzereplo(t2);
-		a.hozzaadSzereplo(t);
-		a.hozzaadSzomszed(b);
-		b.hozzaadSzomszed(a);
-		t.beallitAszteroida(a);
-		t2.beallitAszteroida(b);
-		t.hozzaadNyersanyag(u);
-		t.visszarakNyersanyag(u);
+	static void teszt_epitRobot(String... argumentumok) {
+		Robot r = (Robot) hiv(argumentumok[0], "epitRobot");
+		if (r != null)
+			objektumok.put(argumentumok[1], r);
 	}
 
-	public static void VizjegVisszahelyezes() {
-		Aszteroida a = new Aszteroida(1, true, new Nap(), null);
-		Vizjeg v = new Vizjeg();
-		Telepes t = new Telepes();
-		a.hozzaadSzereplo(t);
-		t.beallitAszteroida(a);
-		t.hozzaadNyersanyag(v);
-		t.visszarakNyersanyag(v);
+	static void teszt_banyaszas(String... argumentumok) {
+		hiv(argumentumok[0], "Banyaszat");
 	}
 
-	public static void mozgasTeleporttal() {
-		Nap nap = new Nap();
-		Aszteroida a = new Aszteroida(3, false, nap, new Szen());
-		Portal p2 = new Portal();
-		Aszteroida b = new Aszteroida(3, false, nap, new Szen());
-		Portal p = new Portal();
-		Telepes t = new Telepes();
-		p.setBirtokos(t);
-		p2.setBirtokos(t);
-		t.beallitAszteroida(a);
-		a.hozzaadSzereplo(t);
-		p.beallitPar(p2);
-		p2.beallitPar(p);
-		p.setVegpont(a);
-		p2.setVegpont(b);
-		a.hozzaadSzomszed(p);
-		t.Mozgas(0); // Ha nem mozgunk akkor is megkérdezi megvan-e az összes nyersanyag.
-						// Rendes játékban ez egy automatikus ellenõrzés lesz ami nem ront el semmit
-						// és nem fogyaszt sok erõforrást így fejlesztõi döntés miatt marad.
+	static void teszt_letrehozNyersanyag(String... argumentumok) {
+		letrehoz(argumentumok[1], argumentumok[0]); // TODO kell-e null mert konsruktor ures
 	}
 
-	public static void uranBanyaszat() {
-		Nap nap = new Nap();
-		Telepes t = new Telepes();
-		Aszteroida a = new Aszteroida(3, false, nap, new Uran());
-		t.beallitAszteroida(a);
-		a.hozzaadSzereplo(t);
-		a.Banyaszat();
+	static void teszt_letrehozAszteroida(String... argumentumok) {
+		letrehoz("Aszteroida", argumentumok[0], "nap");
+		hiv(argumentumok[0], "setReteg", argumentumok[1]);
+		hiv(argumentumok[0], "setNapkozel", argumentumok[2]);
+		hiv(argumentumok[0], "setNyersanyag", argumentumok[3]);
 	}
 
-	public static void vasBanyaszat() {
-		Nap nap = new Nap();
-		Telepes t = new Telepes();
-		Aszteroida a = new Aszteroida(3, false, nap, new Vas());
-		t.beallitAszteroida(a);
-		a.hozzaadSzereplo(t);
-		a.Banyaszat();
-	}
-
-	public static void vizjegBanyaszat() {
-		Nap nap = new Nap();
-		Telepes t = new Telepes();
-		Aszteroida a = new Aszteroida(3, false, nap, new Vizjeg());
-		t.beallitAszteroida(a);
-		a.hozzaadSzereplo(t);
-		a.Banyaszat();
-	}
-
-	public static void szenBanyaszat() {
-		Nap nap = new Nap();
-		Telepes t = new Telepes();
-		Aszteroida a = new Aszteroida(3, false, nap, new Szen());
-		t.beallitAszteroida(a);
-		a.hozzaadSzereplo(t);
-		a.Banyaszat();
-	}
-
-	public static void robotFurasUran() {
-		Robot r = new Robot();
-		Robot r2 = new Robot();
-		Aszteroida a = new Aszteroida(2, false, new Nap(), new Uran());
-		Aszteroida b = new Aszteroida(2, false, new Nap(), new Vas());
-
-		b.hozzaadSzereplo(r2);
-		a.hozzaadSzereplo(r);
-		a.hozzaadSzomszed(b);
-		b.hozzaadSzomszed(a);
-		r.beallitAszteroida(a);
-		r2.beallitAszteroida(b);
-		a.Furas();
-
-	}
-
-	public static void telepesFurasUran() {
-
-		Aszteroida a = new Aszteroida(1, true, new Nap(), new Uran());
-		Aszteroida b = new Aszteroida(1, true, new Nap(), new Szen());
-		Telepes t = new Telepes();
-		Telepes t2 = new Telepes();
-		t2.setPortal(new Portal());
-		t.hozzaadNyersanyag(new Szen());
-		t.setPortal(new Portal());
-		t2.hozzaadNyersanyag(new Szen());
-		b.hozzaadSzereplo(t2);
-		a.hozzaadSzereplo(t);
-		a.hozzaadSzomszed(b);
-		b.hozzaadSzomszed(a);
-		t.beallitAszteroida(a);
-		t2.beallitAszteroida(b);
-
-		a.Furas();
-	}
-
-	public static void Vizjegfuras() {
-		Telepes t = new Telepes();
-		Aszteroida a = new Aszteroida(1, true, new Nap(), new Vizjeg());
-		t.beallitAszteroida(a);
-		t.Furas();
-	}
-
-	public static void Vasfuras() {
-		Telepes t = new Telepes();
-		Aszteroida a = new Aszteroida(1, true, new Nap(), new Vas());
-		t.beallitAszteroida(a);
-		t.Furas();
-	}
-
-	public static void Napvihar() {
-		Nap nap = new Nap();
-		Aszteroida a = new Aszteroida(1, true, nap, new Vas());
-		Telepes t = new Telepes();
-		Robot r = new Robot();
-		t.hozzaadNyersanyag(new Vas());
-		t.setPortal(new Portal());
-		t.beallitAszteroida(a);
-		r.beallitAszteroida(a);
-		a.hozzaadSzereplo(r);
-		a.hozzaadSzereplo(t);
-		nap.hozzaadAszteroidak(new ArrayList<Aszteroida>(Arrays.asList(a)));
-		nap.Lepes();
-	}
-
-	public static void portalkapuEpites() {
-		Telepes t = new Telepes();
-		NyersanyagKoltseg nyk1 = new NyersanyagKoltseg();
-		NyersanyagKoltseg nyk2 = new NyersanyagKoltseg(); // robotéptéshez használt
-		t.hozzaadKoltseg(nyk1);
-		t.hozzaadKoltseg(nyk2);
-
-		t.epitPortal();
-	}
-
-	public static void robotEpites() {
-		Telepes t = new Telepes();
-		NyersanyagKoltseg nyk1 = new NyersanyagKoltseg(); // portalépítéshez használt
-		NyersanyagKoltseg nyk2 = new NyersanyagKoltseg();
-		t.hozzaadKoltseg(nyk1);
-		t.hozzaadKoltseg(nyk2);
-		t.beallitAszteroida(new Aszteroida(0, false, null, null));
-
-		t.epitRobot();
-
-	}
-	
-	static void teszt_letrehozPortalAszteroida(String... argumentumok){
-		letrehoz("Portal",argumentumok[0]);
-        beallit(argumentumok[0],"aszteroida",argumentumok[1]);
-    }
-
-    static void teszt_osszekotAszteroida(String... argumentumok){
-        hiv(argumentumok[0],"hozzaadSzomszed",argumentumok[1]);
-        hiv(argumentumok[1],"hozzaadSzomszed",argumentumok[0]);
-    }
-
-    static void teszt_osszekotPortal(String... argumentumok){
-    	hiv(argumentumok[0],"beallitPar",argumentumok[1]);
-    	hiv(argumentumok[1],"beallitPar",argumentumok[0]);
-    }
-    
-    static void teszt_epitRobot(String... argumentumok) {
-    	Robot r = (Robot)hiv(argumentumok[0],"epitRobot");
-    	if(r!=null)
-    	objektumok.put(argumentumok[1],r );
-    }
-    
-    static void teszt_banyaszas(String... argumentumok) {
-    	hiv(argumentumok[0],"Banyaszat");
-    }
-    
-  
-
-	public static void portalLehelyezes() {
-		Telepes t = new Telepes();
-		Aszteroida a = new Aszteroida(3, false, new Nap(), new Szen()); // telepes helyezkedik el
-																		// rajta
-		Aszteroida a2 = new Aszteroida(3, false, new Nap(), new Szen()); // p2 lesz rajta
-		t.beallitAszteroida(a);
-
-		Portal p1 = new Portal(); // telepesé
-		Portal p2 = new Portal(); // p1 szomszédja
-
-		p1.setBirtokos(t);
-		p2.setBirtokos(t);
-		if (Cin.getBool("Van a telepesnél portal?")) {
-			t.setPortal(p1);
-
-			p1.beallitPar(p2);
-			p2.beallitPar(p1);
-
-			p2.setVegpont(a2);
-
-			t.lehelyezPortal(p1);
+	static void teszt_letrehozTelepes(String... argumentumok) {
+		letrehoz("Telepes", argumentumok[0]); // TODO kell-e null mert konstruktor ures
+		hiv(argumentumok[0], "beallitAszteroida", argumentumok[1]); // aszteroidan is rajta lesz a
+																	// telepes
+		String[] nyersanyagok = Arrays.copyOfRange(argumentumok, 2, argumentumok.length);
+		for (int i = 0; i < nyersanyagok.length; i++) {
+			hiv(argumentumok[0], "hozzaadNyersanyag", nyersanyagok[i]);
 		}
 	}
-	static void teszt_letrehozNyersanyag(String... argumentumok){
-		  letrehoz(argumentumok[1],argumentumok[0]); //TODO kell-e null mert konsruktor ures
-	}
-	
-	static void teszt_letrehozAszteroida(String... argumentumok){
-		  letrehoz("Aszteroida",argumentumok[0],"nap");
-		  hiv(argumentumok[0],"setReteg",argumentumok[1]);
-		  hiv(argumentumok[0],"setNapkozel",argumentumok[2]);
-		  hiv(argumentumok[0],"setNyersanyag",argumentumok[3]);
-	}
-	static void teszt_letrehozTelepes(String... argumentumok){
-		  letrehoz("Telepes",argumentumok[0]); //TODO kell-e null mert konstruktor ures
-		  hiv(argumentumok[0],"beallitAszteroida",argumentumok[1]); //aszteroidan is rajta lesz a telepes
-		  String[] nyersanyagok = Arrays.copyOfRange(argumentumok, 2, argumentumok.length);
-		  for(int i = 0; i<nyersanyagok.length;i++) {
-			  hiv(argumentumok[0],"hozzaadNyersanyag",nyersanyagok[i]); 
-		  }
-	}
-	
-	static void teszt_mozgas(String... argumentumok) {
-    	hiv(argumentumok[0],"Mozgas",); //TODO daninak majd ejfelkor
-	}
-	
+
+	// static void teszt_mozgas(String... argumentumok) {
+	// hiv(argumentumok[0],"Mozgas",); //TODO daninak majd ejfelkor
+	// }
+
 }
