7f255930376ec25acde635ca48c8d5203f66c4c6
Benana6 <szeman.benjamin@gmail.com>
2021-05-02 14:52:14 +0200

    Update TelepesView.java

--- src/TelepesView.java
+++ src/TelepesView.java
@@ -1,5 +1,17 @@
 package src;
 
-public class TelepesView {
+import java.awt.Graphics;
 
+public class TelepesView extends JatekView{
+private Telepes taroltTelepes;
+
+public TelepesView(Telepes t) {
+	taroltTelepes = t;
+}
+
+public void Draw(Graphics g, Integer sorszam) {
+	int x,y,width,height;
+	g.drawRect(x, y, width, height);
+	
+}
 }
