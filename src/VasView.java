24336fc51e52117de94e0cba56b2903604d9d001
Benana6 <szeman.benjamin@gmail.com>
2021-05-02 15:03:21 +0200

    brah

--- src/VasView.java
+++ src/VasView.java
@@ -3,21 +3,14 @@ package src;
 import java.awt.Color;
 import java.awt.Graphics;
 
-public class TelepesView extends JatekView{
-private Telepes taroltTelepes;
-
-public TelepesView(Telepes t) {
-	taroltTelepes = t;
-}
-
+public class VasView {
 public void Draw(Graphics g, Integer sorszam) {
 	int x,y,width,height;
 	x=0;
 	y=0;
 	width=0;
 	height=0;
-	g.setColor(Color.WHITE);
+	g.setColor(Color.LIGHT_GRAY);
 	g.fillRect(x, y, width, height);
-	
 }
 }
