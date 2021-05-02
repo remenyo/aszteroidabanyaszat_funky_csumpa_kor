4fcff7d26fea13855b2f9106bcd804f1e01b1d8c
PoPovok <70849892+PoPovok@users.noreply.github.com>
2021-05-02 18:00:15 +0200

    meretek

--- src/RobotView.java
+++ src/RobotView.java
@@ -4,12 +4,18 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 public class RobotView extends JatekView{
+	
+	static int x = 200;
+	static int y = 250;
+	static int width = 50;
+	static int height = 100;
+	
 	public void Draw(Graphics g, Integer sorszam) {
-		int x,y,width,height;
+		/*int x,y,width,height;
 		x=0;
 		y=0;
 		width=0;
-		height=0;
+		height=0;*/
 		g.setColor(Color.RED);
 		g.fillRect(x, y, width, height);
 		
