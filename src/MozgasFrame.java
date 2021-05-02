e7971180aad6db2ad65b744f90ae1d47e3c8036a
Benana6 <szeman.benjamin@gmail.com>
2021-05-02 16:28:22 +0200

    aa

--- src/MozgasFrame.java
+++ src/MozgasFrame.java
@@ -1,5 +1,34 @@
 package src;
 
-public class MozgasFrame {
+import java.awt.GridLayout;
+import java.awt.event.ItemListener;
+import java.util.ArrayList;
+
+import javax.swing.*;
+
+public class MozgasFrame extends JFrame{
+private JComboBox box;
+private ArrayList<Hely> szomszedok;
+private RajzPanel rajzPanel=new RajzPanel();
+public MozgasFrame(ArrayList<Hely> szomszedok1) {
+	szomszedok = szomszedok1;
+	for (Hely hely : szomszedok) {
+		box.addItem(hely);
+	}
+	setLayout(new GridLayout(2,1));
+	JPanel p1 = new JPanel();
+	this.add(p1);
+	p1.add(box);
+	add(rajzPanel);
+	box.addItemListener(new ItemListener() {
+		public void itemStateChanged(java.awt.event.ItemEvent evt) {
+            rajzPanel.setAszteroida((Hely)box.getSelectedItem());
+        }
+	});
+	}
+
+
+
+
 
 }
