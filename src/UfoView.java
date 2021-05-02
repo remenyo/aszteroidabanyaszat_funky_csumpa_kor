f325c57f13078b08e63eb08e174301ac9a42eb53
Benana6 <szeman.benjamin@gmail.com>
2021-05-02 15:21:20 +0200

    aaaa

---
+++ src/UfoView.java
@@ -0,0 +1,17 @@
+package src;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class UfoView {
+	public void Draw(Graphics g, Integer sorszam) {
+		int x,y,width,height;
+		x=0;
+		y=0;
+		width=0;
+		height=0;
+		g.setColor(Color.GREEN);
+		g.fillRect(x, y, width, height);
+		
+	}
+}
