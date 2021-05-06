ad734f76f12da2ee94da6ac2a02d44379ba8ab1a
remenyo <remenyo@gmail.com>
2021-05-06 20:06:57 +0200

    zsamo

--- src/MozgasFrame.java
+++ src/MozgasFrame.java
@@ -2,7 +2,6 @@ package src;
 
 import java.awt.BorderLayout;
 import java.awt.Button;
-import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemListener;
@@ -11,7 +10,7 @@ import java.util.ArrayList;
 import javax.swing.*;
 
 public class MozgasFrame extends JDialog{
-private JComboBox box = new JComboBox();
+private JComboBox<String> box = new JComboBox<String>();
 private ArrayList<Hely> szomszedok;
 private RajzPanel rajzPanel;
 private Button valasztButton = new Button("Kivalaszt");
--- src/Nap.java
+++ src/Nap.java
@@ -3,22 +3,26 @@ package src;
 import java.util.ArrayList;
 
 /**
- * Aszteroidákat ismer akiken meghívja a napvihart valamilyen valószínűséggel
- * egy kör előrejelzéssel, most tesztelés miatt egyből napvihart hív.
+ * Aszteroidákat ismer akiken meghívja a napvihart valamilyen valószínűséggel egy kör
+ * előrejelzéssel, most tesztelés miatt egyből napvihart hív.
  */
 public class Nap implements Leptetheto {
-	private ArrayList<Aszteroida> aszteroidak = new ArrayList<Aszteroida>(); // Nap körüli aszteroidák
-	private ArrayList<Aszteroida> napviharravarok = new ArrayList<Aszteroida>(); // Akiken meg lesz hívva a napvihar
+	private ArrayList<Aszteroida> aszteroidak = new ArrayList<Aszteroida>(); // Nap körüli
+																				// aszteroidák
+	private ArrayList<Aszteroida> napviharravarok = new ArrayList<Aszteroida>(); // Akiken meg lesz
+																					// hívva a
+																					// napvihar
 	private boolean napviharfolyamatban = false; // Azt jelzi, hogy az előrejelzés van-e
 
 	/**
-	 * Nap lépése, az aszteroidak 5% at kiválasztja és szól nekik madj, következő
-	 * körben meghívja rajtuk.
+	 * Nap lépése, az aszteroidak 5% at kiválasztja és szól nekik madj, következő körben meghívja
+	 * rajtuk.
 	 */
 	public void Lepes() {
 		Log.call();
 		if (!napviharfolyamatban) {
-			if (RandomUtils.randomIntHatarokKozott(0, 100) <= 5) {
+			if (false && /* <--tesztek alatt van ez csak TODO */ RandomUtils
+					.randomIntHatarokKozott(0, 100) <= 5) {
 				napviharravarok.clear();
 				for (int i = 0; i < aszteroidak.size() / 2; i++) {
 					Aszteroida randomNapviharravaro = aszteroidak
